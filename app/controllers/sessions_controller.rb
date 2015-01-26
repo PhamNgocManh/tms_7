29169863108c124f34b0d00671ad4ff2f5d3731a
dinhvantiep <dinhvantiep2009@gmail.com>
2015-01-26 16:56:04 +0700

    Task/25935+25973 Log in/ log out

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by email: params[:session][:email].downcase
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      user.require_password = false
+      params[:session][:remember_me] == "1" ? remember(user) : forget(user)
+      user.require_password = true
+      redirect_to user_url(user)
+      # Log the user 
+    else
+      flash.now[:danger] ="Invalid email/password combination"
+      render "new"
+    end
+  end
+
+  def destroy
+    log_out if logged_in?
+    redirect_to root_url
+  end
+
+end
